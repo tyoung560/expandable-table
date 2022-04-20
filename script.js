b4fae4fb7fd9155cdf8aa68d342804b6de42b510
tyoung560 <47755919+tyoung560@users.noreply.github.com>
2022-04-19 23:31:44 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {    
-    //hides articles
-    $(".expandable-row").hide();  
-    //article accordian
-    $(".show-details").click(function() {
-        $(this).parent().siblings(".expandable-row").slideToggle("slow");
-    });
-});
